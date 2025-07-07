36a4c5d87aacd32f4654300c13df07502780e034
cheolhee <cheolhee.jung@a-bly.com>
2025-07-07 11:04:54 +0900

    주석 변경

--- src/main/kotlin/router/WebSocketRouter.kt
+++ src/main/kotlin/router/WebSocketRouter.kt
@@ -11,9 +11,9 @@ import java.util.*
 
 /**
  * workflow:
- * 1. 소켓이 연결 될 때 마다 전역 변수인 SocketPoolSize에 연결된 소켓의 개수를 저장합니다.
- * 2. 1초마다 스케줄러가 돌면서 SocketPoolSize를 확인하고, redis에 increment 합니다.
- * 3. 소켓이 연결이 끊어지면 SocketPoolSize를 감소시키고, redis에 decrement 합니다.
+ * 1. 소켓이 연결 될 때 마다 전역 변수인 currentSocketConnections에 연결된 세션을 저장합니다.
+ * 2. 1초마다 스케줄러가 돌면서 currentSocketConnections를 확인하고, redis에 bulk increment 합니다.
+ * 3. 소켓이 연결이 끊어지면 currentSocketConnections에서 세션을 삭제하고, redis에 bulk decrement 합니다.
  * 4. 먀번 increment, decrement 하면 레디스가 오버헤드를 많이 받기 때문에
  *    이전 SocketPoolSize와 현재 SocketPoolSize를 비교해서 increment 할 지 decrement 할 지 결정합니다.
  * 5. 레디스에 increment, decrement가 끝나면 socket에 연결된 사용자들에게 현재 동접자가 몇명인지 발송해줍니다.
