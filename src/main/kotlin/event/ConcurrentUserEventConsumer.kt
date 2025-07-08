913f36b88907bbb85149491d139a44dccdd471c7
cheolhee <cheolhee.jung@a-bly.com>
2025-07-08 21:48:47 +0900

    병렬처리 추가

--- src/main/kotlin/event/ConcurrentUserEventConsumer.kt
+++ src/main/kotlin/event/ConcurrentUserEventConsumer.kt
@@ -3,24 +3,28 @@ package com.example.event
 import com.example.external.ReadRedisService
 import com.example.model.currentSocketConnections
 import io.github.oshai.kotlinlogging.KotlinLogging
-import io.ktor.websocket.Frame
+import io.ktor.websocket.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 
 class ConcurrentUserEventConsumer(
     private val readRedisService: ReadRedisService,
-): EventConsumer {
+) : EventConsumer {
     private val logger = KotlinLogging.logger {}
 
-    override suspend fun receiveEvent() {
+    override suspend fun receiveEvent() = supervisorScope {
         val event = concurrentUserChannel.receive()
         val concurrentUserCount = readRedisService.get(event.key)?.toLong() ?: 0
 
         currentSocketConnections.forEach { connection ->
-            try {
-                connection.session.send(
-                    Frame.Text("동접자 수: $concurrentUserCount")
-                )
-            } catch (e: Exception) {
-                logger.error(e) { "session id: [${connection.sessionId}]에 메시지 전송 실패" }
+            launch {
+                try {
+                    connection.session.send(
+                        Frame.Text("동접자 수: $concurrentUserCount")
+                    )
+                } catch (e: Exception) {
+                    logger.error(e) { "session id: [${connection.sessionId}]에 메시지 전송 실패" }
+                }
             }
         }
     }
