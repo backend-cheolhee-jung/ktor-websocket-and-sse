500ae3917910d8e48fece73758d7aceb4c79282d
lolmageap <ekxk1234@naver.com>
2025-07-13 15:41:20 +0900

    중복 이벤트 방지를 위한 id value 추가

--- src/main/kotlin/event/ConcurrentUserEvent.kt
+++ src/main/kotlin/event/ConcurrentUserEvent.kt
@@ -1,6 +1,30 @@
 package com.example.event
 
-@JvmInline
-value class ConcurrentUserEvent(
-    val key: String,
-)
+import java.util.concurrent.locks.ReentrantLock
+
+class ConcurrentUserEvent(
+    val key: String
+) {
+    val id: Long = ConcurrentUserEventAutoIncrementBuilder.next()
+
+    override fun equals(other: Any?): Boolean =
+        other is ConcurrentUserEvent && key == other.key && id == other.id
+
+    override fun hashCode(): Int = 31 * key.hashCode() + id.hashCode()
+
+    override fun toString(): String = "ConcurrentUserEvent(key=$key, id=$id)"
+}
+
+object ConcurrentUserEventAutoIncrementBuilder {
+    private val lock = ReentrantLock()
+    private var currentId: Long = 0
+
+    fun next(): Long {
+        lock.lock()
+        try {
+            return currentId++
+        } finally {
+            lock.unlock()
+        }
+    }
+}
